7ca70085032f718a1d8ee7661871a3262c1d2cac
stevekutz <stkutz@gmail.com>
2020-11-09 13:53:44 -0600

    created comp to render API data

--- app/src/App.js
+++ app/src/App.js
@@ -183,11 +183,31 @@ const comic_info = {
 }
 
 function App() {
+
+  const metaData = comic_info.data.results[0];
+
+  // destructure
+  const {thumbnail, title, issueNumber, pageCount, dates, prices, creators} = metaData
+
+
+
+
   	return (
-    	<div className="App">
-			<h1> {comic_info.data.results[0].title} </h1>
-			<img src = {`${comic_info.data.results[0].thumbnail.path}.${comic_info.data.results[0].thumbnail.extension}`}/>
-    	</div>
+    	<div className="comic">
+            <div className = "comic-thumbnail-container">
+    	        <img className = "comic-thumbnail" src = {`${thumbnail.path}.${thumbnail.extension}`} alt = 'comic'/>
+            </div>
+            <div className = "comic-data-container">
+                <h1> {title} </h1>
+                <p> issue: {issueNumber} </p>
+                <p> pages: {pageCount} </p>
+                <p> Date: {new Date(dates[0].date).toLocaleDateString()} </p>
+                <p> Price: ${prices[0].price} </p>
+            
+            </div>
+			{/* <img src = {`${comic_info.data.results[0].thumbnail.path}.${comic_info.data.results[0].thumbnail.extension}`}/> */}
+            {/* <img src = {`${metaData.images[0].path}.${longPath.images[0].extension}`} alt = 'comic'/>   */}
+      </div>
   );
 }
 
